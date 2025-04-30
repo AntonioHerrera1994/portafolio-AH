bcae391f0ded234d9cb48e5f587f622e3ba8ad50
Antonio Herrera <herreraquinteroj@gmail.com>
2025-04-30 10:47:10 -0700

    port

--- src/App.jsx
+++ src/App.jsx
@@ -86,7 +86,7 @@ function App() {
 </section>
 
 <section id="projects" className="py-16 px-6 max-w-4xl mx-auto" data-aos="fade-up">
-  <h2 className="text-3xl font-semibold mb-8">Portafolio</h2>
+  <h2 className="text-3xl font-semibold mb-8">Proyectos</h2>
   <div className="grid gap-6 md:grid-cols-2">
     <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
       <h3 className="text-xl font-semibold">Portafolio Profesional</h3>
@@ -96,7 +96,7 @@ function App() {
 
     </div>
     <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
-      <h3 className="text-xl font-semibold">Dashboard Admin</h3>
+      <h3 className="text-xl font-semibold">Portafolio Personal</h3>
       <p className="text-gray-400 mt-2">Proyectos personales que he realizado con tecnológias tales como Astro y React.</p>
       <Link to="/projectpersonal" className="text-cyan-400 text-sm underline mt-2 inline-block">Ver Proyectos</Link>
     </div>
